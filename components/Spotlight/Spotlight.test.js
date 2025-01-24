a2631ac50346e2003a3cc49815d065d3cf342582
Herr <g.kerber@posteo.it>
2025-01-25 00:44:23 +0100

    fix wrong name in spotlight.test.js

--- components/Spotlight/Spotlight.test.js
+++ components/Spotlight/Spotlight.test.js
@@ -4,7 +4,7 @@ import Spotlight from ".";
 // render + screen
 
 test("Renders a Spotlight component", () => {
-  render(<Movie name="Tatort Folge 18001" />);
+  render(<Spotlight name="Tatort Folge 18001" />);
 
   // const heading = screen.getByText("Tatort Folge 18002");
   const heading = screen.getByRole("heading", { name: "Tatort Folge 18001" });
